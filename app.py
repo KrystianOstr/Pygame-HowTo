114d30d5ca2c6ad267ac7077e5784efb80d4b4a1
Krystian <krystian.ostrogorski@gmail.com>
2025-06-09 19:17:27 +0200

    added mouse movement

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 import pygame
+
 pygame.init()
 
 SCREEN_WIDTH = 700
 SCREEN_HEIGHT = 700
+SPEED = 1.3
 
-screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT),0,32)
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
 
 
-sprite_butterfly = pygame.image.load('images/butterfly.png')
-sprite_butterfly = pygame.transform.scale(sprite_butterfly, (32,32))
+sprite_butterfly = pygame.image.load("images/butterfly.png")
+sprite_butterfly = pygame.transform.scale(sprite_butterfly, (32, 32))
 
 sprite_butterfly_width = sprite_butterfly.get_width()
 sprite_butterfly_height = sprite_butterfly.get_height()
 
-pygame.display.set_caption('Pygame HowTo')
-screen.fill((0,0,0))
+pygame.display.set_caption("Pygame HowTo")
+screen.fill((0, 0, 0))
 game_over = False
 
-x,y = (0,0)
+x, y = (0, 0)
 
 clock = pygame.time.Clock()
 
@@ -29,26 +31,34 @@ while not game_over:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 game_over = True
-            
+
+        elif event.type == pygame.MOUSEMOTION:
+            x, y = event.pos
+            x -= sprite_butterfly_width / 2
+            y -= sprite_butterfly_height / 2
+
     pressed = pygame.key.get_pressed()
     if pressed[pygame.K_UP]:
-        y-=1*dt
+        y -= SPEED * dt
     if pressed[pygame.K_DOWN]:
-        y+=1*dt
+        y += SPEED * dt
     if pressed[pygame.K_LEFT]:
-        x-=1*dt
+        x -= SPEED * dt
     if pressed[pygame.K_RIGHT]:
-        x+=1*dt
+        x += SPEED * dt
     if pressed[pygame.K_SPACE]:
         x = 0
         y = 0
-            
+    if pressed[pygame.K_F1]:
+        SPEED = 1
+    if pressed[pygame.K_F2]:
+        SPEED = 2
 
-    if x > (screen.get_width() +sprite_butterfly_width):
+    if x > (screen.get_width() + sprite_butterfly_width):
         x = 0 - sprite_butterfly_width
 
     if x < 0 - sprite_butterfly_width:
-        x = screen.get_width()-sprite_butterfly_width
+        x = screen.get_width() - sprite_butterfly_width
 
     if y < 0 - sprite_butterfly_width:
         y = screen.get_height() + sprite_butterfly_height
@@ -56,8 +66,11 @@ while not game_over:
     if y > screen.get_height() + sprite_butterfly_height:
         y = 0 - sprite_butterfly_height
 
-    screen.fill((0,0,0))
-    screen.blit(sprite_butterfly, (x, y))
+    screen.fill((0, 0, 0))
+    screen.blit(
+        sprite_butterfly,
+        (x, y),
+    )
 
     pygame.display.update()
 
