4bcbc3396f8e89ff496f654d7c8d31f63323c451
Krystian <krystian.ostrogorski@gmail.com>
2025-06-20 15:51:21 +0200

    added ball and collisions

--- BreakinBricks.py
+++ BreakinBricks.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.locals import *
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
@@ -12,6 +13,11 @@ bat_rect[1] = screen.get_height() - 100
 # ball
 ball = pygame.image.load("images/football.png").convert_alpha()
 ball_rect = ball.get_rect()
+ball_start = (200,200)
+ball_speed = (3.0,3.0)
+ball_served = False
+sx, sy = ball_speed
+ball_rect.topleft = ball_start
 
 # brick
 
@@ -36,6 +42,7 @@ for y in range(brick_rows):
 
 clock = pygame.time.Clock()
 run = True
+x = 0
 
 while run:
     dt = clock.tick(50)
@@ -43,6 +50,9 @@ while run:
 
     for b in bricks:
         screen.blit(brick, b)
+
+    screen.blit(bat, bat_rect)
+    screen.blit(ball, ball_rect)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -53,11 +63,43 @@ while run:
 
     pressed = pygame.key.get_pressed()
     if pressed[pygame.K_LEFT]:
-        bat_rect[0] -= 1
+        x -= 1 * dt
     if pressed[pygame.K_RIGHT]:
-        bat_rect[0] += 1
+        x += 1 * dt
+    if pressed[pygame.K_SPACE]:
+        ball_served = True
 
-    screen.blit(bat, (screen.get_width() // 2, bat_rect[1]))
+
+    # if ball_rect.bottom >= bat_rect.top and \
+    # ball_rect.top < bat_rect.top and \
+    # ball_rect.right >= bat_rect.left and \
+    # ball_rect.left <= bat_rect.right:
+    #     sy *= -1
+
+    if bat_rect[0] + bat_rect.width >= ball_rect[0] >= bat_rect[0] and \
+    ball_rect[1] + ball_rect.height >= bat_rect[1] and \
+    sy > 0:
+        sy *= -1
+        continue
+    
+    #screen restriction  
+    if ball_rect.top <= 0:
+        sy *=-1
+    if ball_rect.bottom >= screen.get_height():
+        sy *=-1
+    if ball_rect.left <= 0:
+        sx*=-1  
+    if ball_rect.right >= screen.get_width():
+        sx*=-1
+
+
+
+    bat_rect[0] = x
+    if ball_served:
+        ball_rect[0] += sx
+        ball_rect[1] += sy
+ 
+
     pygame.display.update()
 
 pygame.quit()
