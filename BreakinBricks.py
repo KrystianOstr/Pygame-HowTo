4f5f12196dc4d776b980fb3994d39d2cb7f2121b
Krystian <krystian.ostrogorski@gmail.com>
2025-06-25 09:22:44 +0200

    added ai, added colisions

--- BreakinBricks.py
+++ BreakinBricks.py
@@ -4,6 +4,8 @@ from pygame.locals import *
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Breakin' Bricks")
+PLAYER_MODE_LIST = ['PLAYER','AI']
+PLAYER_MODE = PLAYER_MODE_LIST[1]
 
 # bat
 bat = pygame.image.load("images/paddle.png").convert_alpha()
@@ -13,7 +15,7 @@ bat_rect[1] = screen.get_height() - 100
 # ball
 ball = pygame.image.load("images/football.png").convert_alpha()
 ball_rect = ball.get_rect()
-ball_start = (200,200)
+ball_start = (250,250)
 ball_speed = (3.0,3.0)
 ball_served = False
 sx, sy = ball_speed
@@ -62,43 +64,67 @@ while run:
                 run = False
 
     pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_LEFT]:
-        x -= 1 * dt
-    if pressed[pygame.K_RIGHT]:
-        x += 1 * dt
+    if PLAYER_MODE == 'PLAYER':
+        if pressed[pygame.K_LEFT]:
+            x -= 1 * dt
+        if pressed[pygame.K_RIGHT]:
+            x += 1 * dt
     if pressed[pygame.K_SPACE]:
         ball_served = True
 
+    if PLAYER_MODE == 'AI':
+        bat_rect.centerx = ball_rect.centerx
+        
 
-    # if ball_rect.bottom >= bat_rect.top and \
-    # ball_rect.top < bat_rect.top and \
-    # ball_rect.right >= bat_rect.left and \
-    # ball_rect.left <= bat_rect.right:
-    #     sy *= -1
 
     if bat_rect[0] + bat_rect.width >= ball_rect[0] >= bat_rect[0] and \
     ball_rect[1] + ball_rect.height >= bat_rect[1] and \
     sy > 0:
+        print('PADDLE HIT')
         sy *= -1
+        sx *= 1.01
+        sy *= 1.01
         continue
+
+
+    delete_brick = None
+    for b in bricks:
+        bx,by = b
+        if bx <= ball_rect[0] <= bx + brick_rect.width and \
+         by <= ball_rect[1] <= by + brick_rect.height :
+            delete_brick = b
+
+            if ball_rect[0] <= bx + 2:
+                sx*= -1
+            elif ball_rect[0] >= bx + brick_rect.width - 2:
+                sx *= -1
+            if ball_rect[1] <= by + 2:
+                sy*=-1
+            elif ball_rect[1] >= by + brick_rect.height -2:
+                sy*=-1
+            break
+
+    if delete_brick is not None:
+        bricks.remove(delete_brick)
     
     #screen restriction  
     if ball_rect.top <= 0:
         sy *=-1
     if ball_rect.bottom >= screen.get_height():
-        sy *=-1
+        ball_served = False
+        ball_rect.topleft = ball_start
     if ball_rect.left <= 0:
         sx*=-1  
     if ball_rect.right >= screen.get_width():
         sx*=-1
 
 
-
-    bat_rect[0] = x
+    if PLAYER_MODE == 'PLAYER':
+        bat_rect[0] = x
     if ball_served:
         ball_rect[0] += sx
         ball_rect[1] += sy
- 
+
 
     pygame.display.update()
 
